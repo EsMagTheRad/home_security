dd351b74c2cadd442c8deb4be6f5bd4d139a67fb
EsMagThrRad <shion.maggio@stud.hshl.de>
2017-06-04 17:56:53 +0200

    #007 Added tooltip and mouseoverevents to Barchart

--- table/script/chart.js
+++ table/script/chart.js
@@ -1,4 +1,4 @@
-var dataArray = [20,25,15,19,23,26,30,33,36,39,41,43]; //Werte für Balken
+var dataArray = [20,25,15,19,23,26,30,33,37,42,39,38]; //Werte für Balken
 var dataMonth = ['Jan','Feb','Mar','Apri','Mai','Jun','Jul','Aug','Sep','Oct','Nov','Dez'];//Strings
                 //Beschriftung YAchse
 var dataYAxis = [50,45,40,35,30,25,20,15,10,5];
@@ -12,15 +12,24 @@ var y = d3.scaleOrdinal()
             .domain(dataYAxis)
             //Distanz zwischen den Werten der der Y-Achse
             .range([5*s,10*s,15*s,20*s,25*s,30*s,35*s,40*s,45*s,50*s,55*s]); 
-
-
+ 
 var xAxis = d3.axisBottom(x);
 var yAxis = d3.axisLeft(y);
 var yPos = 606;//Change for y positioning of XAxis
-var margin = {left:120,right:0,top:55,bottom:0} //For global margin
+var margin = {left:50,right:0,top:55,bottom:0} //For global margin
 
-var svg = d3.select("body").append("svg").attr("height","1000").attr("width","100%");
+
+var svg = d3.select("body").append("svg").attr("height","700").attr("width","100%");
 var chartGroup = svg.append("g").attr("transform","translate("+margin.left+","+margin.top+" )")
+
+
+var tooltip = d3.select('body').append('div')
+                .style('position', 'absolute')
+                .style('padding','10px 20px')
+                .style('background', 'DarkKhaki')
+                .style('opacity', 0)
+                .style('color', 'white')
+                .style('border-radius', '5px')
 
 chartGroup.selectAll("rect")
             .data(dataArray)
@@ -29,7 +38,28 @@ chartGroup.selectAll("rect")
                 .attr("width","30")//Breite der Balken
                 .attr("fill","pink")//Farbe der Balken
                 .attr("x",function(d,i){ return 40*i; })//Abstand zwischen dern Balken (hier 10 pixel)
-                .attr("y",function(d,i){ return yPos-(d*10); });
+                .attr("y",function(d,i){ return yPos-(d*10); })
+//Mouseoverevents
+
+            .on('mouseover', function(d){
+    
+                tooltip.transition()
+                    .style('opacity', 1)
+                
+                tooltip.html(d)
+                    .style('left', (d3.event.pageX - 10) + 'px')
+                    .style('top', (d3.event.pageY - 45) + 'px')
+                oldcolor = this.style.fill;
+                d3.select(this)
+                    .style('fill', 'purple')
+                    .style('opacity', 0.5)
+            })
+              .on('mouseout', function(d){
+                d3.select(this)
+                    .style('fill', 'pink')
+                    .style('opacity', 1)
+            });
+
 chartGroup.append('g')
         .attr("class","x axis")
         .attr("transform","translate(0, "+ yPos +" )")
